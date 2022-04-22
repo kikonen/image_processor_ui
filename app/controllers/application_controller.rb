ecb454c228013707ceb17d87ca1ea2165d36ef3e
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-04-23 01:17:58 +0300

    FIX handle expired session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   include AuthorizationSupport
 
+  rescue_from JWT::ExpiredSignature, with: :response_unauthenticated
+
   before_action :require_authorization
+
+  def response_unauthenticated
+    redirect_to '/auth/login'
+  end
 end
